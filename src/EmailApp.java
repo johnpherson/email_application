ab77e7a84075ec3812e174eb41a423ff0a205e3f
johnpherson <phersonj@gmail.com>
2020-03-30 20:48:06 -0600

    intitial commit

---
+++ src/EmailApp.java
@@ -0,0 +1,15 @@
+public class EmailApp {
+    public static void main(String[] args) {
+
+    }
+
+    public class Email
+    {
+        String firstName;
+        String lastName;
+        String password;
+        String department;
+        int mailboxCapacity;
+        String alternateEmail;
+    }
+}
